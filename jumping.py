a2c8728ac38df26918fb229711da821b15b75815
Nithin Tatikonda <nithin0540@gmail.com>
2025-11-03 14:30:18 +0530

    first modification on jumping.py

---
+++ jumping.py
@@ -0,0 +1,25 @@
+'''
+pass = ignore
+continue = skip
+break = terminate loop
+
+'''
+
+if True:
+    pass
+
+
+
+for i in "pythonlife":
+    if i == 'l':
+        continue
+    print(i)
+
+print("-------------")
+
+for i in "pythonlife":
+    if i == 'l':
+        break
+    print(i)
+
+
