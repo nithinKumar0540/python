97f1c44e95c670552599af74cc73cb543eb78977
Nithin Tatikonda <nithin0540@gmail.com>
2025-11-03 18:39:29 +0530

    added new file typeconversion.py

---
+++ typeconversion.py
@@ -0,0 +1,35 @@
+'''
+typeconvertion
+
+No data loss implicit
+data loss explicit
+
+int to float
+float to int
+str to int
+
+str to float
+
+str to list
+str to tuple
+str to set
+'''
+
+a = 10
+print(float(a))
+
+b = 20.3
+print(int(b))
+
+c = 'Hello Hi This Is Nithin'
+d = '1, 20, 5, 23, 10 '
+e = '5'
+
+f = "10.5"
+print(int(e))
+print(list(c))
+print(tuple(d))
+print(float(f))
+# str to list
+g = 'True, 1,1,12.2,"Hello", 14'
+print(list(g))
