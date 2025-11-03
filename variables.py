b21ec695ad08c4c7375107155033e89da3abb383
Nithin Tatikonda <nithin0540@gmail.com>
2025-11-03 18:01:53 +0530

    added new file variable.py

---
+++ variables.py
@@ -0,0 +1,19 @@
+#variables
+'''
+int()
+float()
+bool()
+str()
+complex()
+
+'''
+a = 10
+b = 20.5
+c = True
+d = "Hi Hello"
+e = 5 + 2J
+
+print(a,b,c,d,e)
+print(id(a), id(b), id(c), id(d), id(e) )
+print(type(a), type(b), type(c), type(d), type(e) )
+
