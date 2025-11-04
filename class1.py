d5ec654eaed3d7905123f9175bd23cb7db48b282
Nithin Tatikonda <nithin0540@gmail.com>
2025-11-04 20:26:41 +0530

    added python -u d:programspythonhierarchicalInheritance.pyclass1 and class2

---
+++ class1.py
@@ -0,0 +1,11 @@
+class student():
+    name = "Nithin"
+    age = 23
+    college = "Narayana"
+    marks = 95.5
+
+s1 = student()
+print(s1.name)
+print(s1.age)
+print(s1.college)
+print(s1.marks)
---
+++ class2.py
@@ -0,0 +1,15 @@
+class pythonlife():
+    a = 15
+    print(a)
+
+
+print("---------------")
+
+class pythonlife1():
+    b = 23
+    def output(self):
+        print(self.b)
+#when you have methods in class you have to create object to class
+s1 = pythonlife1()
+s1.output()
+
